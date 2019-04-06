1c03b0fc22ccc27be5fdb5e61aa198d7344ae8f3
Withoutmelv <781033189@qq.com>
2019-04-06 08:37:21 +0800

    save

--- whack-a-mole/game-normal.js
+++ whack-a-mole/game-normal.js
@@ -10,6 +10,7 @@ window.onload = function () {
     let timeUp = false;
     let score = 0;
     let gameTime = 10000;
+    let norepeat=[0,0,0,0,0,0];
 
 
     startBtn.addEventListener('click', function () {
@@ -43,6 +44,12 @@ window.onload = function () {
     function startGame() {
         if(!timeUp){
             let i=Math.round(Math.random()*5);
+            let j=Math.round(Math.random()*300);
+            if (norepeat[i]===1){
+                norepeat[i]=0;
+                i=(i+1)%6;
+            }
+            norepeat[i]=1;
             let num=(i+1).toString();
             holes[i].classList.remove('hole'+num);
             holes[i].classList.add('up');
@@ -50,13 +57,13 @@ window.onload = function () {
             setTimeout(()=>{
                 holes[i].classList.remove('up');
                 holes[i].classList.add('hole'+num);
-            },400);
+            },400+j);
             setTimeout(()=>{
                 moles[i].removeEventListener('click',addscore,false);
-            },800);
+            },800+j);
             setTimeout(()=>{
                 startGame();
-            },400);
+            },400+j);
         }
     }
 
